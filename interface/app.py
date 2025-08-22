47235fa566ae9a14bec9be610177450b22db8d0d
Numantic-NMoroney <nathan.moroney@numanticsolutions.com>
2025-08-22 08:36:59 -0700

    design edits

--- interface/app.py
+++ interface/app.py
@@ -36,8 +36,7 @@ vertexai.init(project=os.environ["GOOGLE_CLOUD_PROJECT"],
 
 ########## Set up Streamlit
 st.set_page_config(page_title="CCC-PA")
-font_url = ("https://fonts.googleapis.com/css2?family=Lato:ital,wght"
-            "@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap")
+font_url="https://fonts.googleapis.com/css?family=Roboto"
 streamlit_style = """
 			<style>
 			@import url({font_url});
